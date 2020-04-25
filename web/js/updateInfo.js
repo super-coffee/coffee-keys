9172dfd2d1f9a3e1d9a10fa86bc697696c9a3a70
jinzhijie <742905606@qq.com>
2020-04-25 14:09:00 +0800

    完善 updateInfo

--- web/js/updateInfo.js
+++ web/js/updateInfo.js
@@ -1,36 +1,32 @@
-var verifyPassword = new Vue({
-    el: "#verifyPassword",
-    data: {
-        opt_widget_id: null
-    }
-});
-var checkPassword = new Vue({
-    el: '#checkPassword',
-    data: {
-        password: "",
-        repeat_password: ""
-    },
-    computed: {
-        // 计算属性的 getter
-        isEqual: function () {
-            // `this` 指向 vm 实例
-            return this.password == this.repeat_password;
-        }
-    }
-});
-var updateInfo = new Vue({
-    el: "#updateInfo",
-    data: {
-        name: null,
-        mail: null,
-        pubkey: null
-    }
-});
+var opt_widget_id;
 
 var twoform = new Vue({
     el: "#twoform",
     data: {
-        canShow: false
+        hiddenData: false,
+        canShow: false,
+        origin: {
+            mail: null,
+            password: null
+        },
+        updateInfo: {
+            name: null,
+            mail: null,
+            pubkey: null
+        },
+        checkPassword: {
+            password: "",
+            repeat_password: ""
+        }
+    },
+    computed: {
+        hasPassword: function () {
+            return this.checkPassword.password == "" ? false : true;
+        },
+        isPasswordEqual: function () {
+            // `this` 指向 vm 实例
+            return this.checkPassword.password == this.checkPassword.repeat_password;
+        }
     }
 });
 
@@ -38,12 +34,11 @@ function recaptchainVerifyPassword() {
     axios
         .get(`/api/recaptcha/getSiteKey`)
         .then(response => {
-            verifyPassword.opt_widget_id = grecaptcha.render(
+            opt_widget_id = grecaptcha.render(
                 "recaptchainVerifyPassword",
                 { "sitekey": response.data }
             );
-            console.log(verifyPassword.opt_widget_id);
-            grecaptcha.reset(verifyPassword.opt_widget_id);
+            grecaptcha.reset(opt_widget_id);
         })
         .catch(error => {
             console.log(error);
@@ -77,12 +72,13 @@ layui.use('form', function () {
         if (canSubmit) {
             var u_mail = data["mail"];
             var u_password = data["password"];
-            console.log(u_password);
             var g = data["g-recaptcha-response"];
             axios
                 .get(`/api/verifyPassword?mail=${u_mail}&password=${u_password}&g-recaptcha-response=${g}`)
                 .then(response => {
                     if (response.data.status) {
+                        // 显示修改的信息框
+                        searchKey(twoform.origin.mail);
                         twoform.canShow = true;
                         grecaptcha();
                     } else {
@@ -91,7 +87,7 @@ layui.use('form', function () {
                             layer.msg(response.data.data);
                         });
                     }
-                    grecaptcha.reset(verifyPassword.opt_widget_id);
+                    grecaptcha.reset(opt_widget_id);
                 })
                 .catch(error => {
                     console.log(error);
@@ -99,7 +95,7 @@ layui.use('form', function () {
                         var layer = layui.layer;
                         layer.msg("发生了一个异常，请到 Console 查看");
                     });
-                    grecaptcha.reset(verifyPassword.opt_widget_id);
+                    grecaptcha.reset(opt_widget_id);
                 });
         }
         return false;  // true → 跳转；false → 不跳转
@@ -118,13 +114,13 @@ layui.use('form', function () {
         // 判断是否可提交
         if (!doRecaptcha || !hasRecaptcha) {
             layui.use('layer', function () {
-                layer.tips('请完成 reCAPTCHA 人机身份验证', '#submit', {
+                layer.tips('请完成 reCAPTCHA 人机身份验证', '#submit2', {
                     tips: 3,  // 下
                     tipsMore: true
                 });
             });
         };
-        if (!checkPassword.isEqual) {
+        if (!twoform.isPasswordEqual) {
             layui.use('layer', function () {
                 layer.tips('密码不一致', '#repeat-password', {
                     tips: 3,  // 下
@@ -132,9 +128,30 @@ layui.use('form', function () {
                 });
             });
         };
-        if (doRecaptcha && checkPassword.isEqual) {
+        if (doRecaptcha && twoform.isPasswordEqual && hasRecaptcha) {
             canSubmit = true;
         };
+        console.log(doRecaptcha);
+        console.log(hasRecaptcha);
+        console.log(twoform.isPasswordEqual);
         return canSubmit;  // true → 跳转；false → 不跳转
     });
 });
+
+// searchKey
+function searchKey(userMail) {
+    axios
+        .get(`/api/searchKey?mail=${userMail}`)
+        .then(response => {
+            responseData = response.data;
+            if (responseData.status) {
+                twoform.updateInfo = responseData.data;
+            } else {
+                layer.msg(responseData.data);
+            };
+        })
+        .catch(error => {
+            console.log(error);
+            layer.msg("发生了一个异常，请到 Console 查看");
+        });
+};
